244d8ac69ca30c5fe0e7bec35316fe8e527e0e55
akash <pratap_the_knight@yahoo.com>
2023-01-28 22:14:17 -0500

    updated the wdio conf file to run inside selenium grid

---
+++ test/features/demo/WebInteractions.feature
@@ -0,0 +1,10 @@
+Feature: Web Interactions
+
+    @demo
+    Scenario Outline: DemoWeb Interaction
+        Given A web page is opened
+        When Perform web interaction
+
+        Examples:
+            | TestID    |
+            | WEB_TC002 |
--- test/features/step-definations/demo.ts
+++ test/features/step-definations/demo.ts
@@ -24,3 +24,43 @@ Then(/^URL should match (.*)$/, async function(expectedURL){
     let url = await browser.getUrl()
     chai.expect(url).to.equal(expectedURL)
 })
+
+/** Web Iteraction */
+
+Given(/^A web page is opened$/, async function(){
+    await browser.url("/inputs")
+    await browser.setTimeout({implicit: 15000, pageLoad: 10000})
+    await browser.maximizeWindow()
+
+})
+
+When(/^Perform web interaction$/, async function() {
+    /** 
+     * 1. Input Box
+     * Actions :
+     * 1. Type into input box
+     * 2. Clear the field and type or just add value
+     * 3. Click and type
+     * 4. Slow typing
+     * 
+     */
+
+    let ele = await $(`[type=number]`)
+    await ele.setValue("12345")
+    await browser.pause(3000)
+    
+
+    // let num = 12345
+    // let strNum = num.toString
+
+    // let ele = await $(`[type=number]`)
+    // await ele.click()
+    // for (let i=0; i < strNum.length; i++) {
+    //     let charSet = strNum.charAt(i)
+    //     await browser.pause(1000)
+    //     await browser.keys(charStr)
+    // }
+
+    // await browser.debug()
+
+})
